a2dceab17e3b13b990f125693283de863e816ee5
Alan Paredes <alanpar97@gmail.com>
2025-05-21 15:07:29 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
-"""Python setup.py for project_name package"""
-import io
-import os
-from setuptools import find_packages, setup
+"""Python setup.py for certifai package"""
+from setuptools import setup
 
 req = ["numpy",
 "pandas",
@@ -9,21 +7,12 @@ req = ["numpy",
 "tqdm"
 ]
 
-def read_requirements(path):
-    return [
-        line
-        for line in read(path).splitlines()
-        if line                                   # <-- guard against blanks
-        and not line.startswith(("#", "-", "git+"))
-    ]
-
-
 setup(
     name="certifai",
     version="0.1.0",
     description="A python implementation of CERTIFAI framework for machine learning models' explainability",
     url="https://github.com/alanpar97/CERTIFAI/",
     author="Alan Paredes",
-    packages=find_packages(exclude=["tests", ".github"]),
+    packages="certifai",
     install_requires=req,
 )
